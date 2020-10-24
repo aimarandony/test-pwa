689c78f85e21e51e70c023c068d5c9506fa6b7b5
Aimar Andony <aimarandony10@gmail.com>
2020-10-24 12:56:34 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pepe from './images/el-pepe.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pepe} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PWA!!! EL PEPE
         </p>
         <a
           className="App-link"
